f8c95b9c8f594906c53c097b4b9dce62dc143cbf
jdczw <jdczw@DESKTOP-SR2D12F.realtek>
2019-11-05 22:08:05 +0800

    implementation on user story id #169496328 : view student detail

--- DSA search Engine/src/DataClass/Student.java
+++ DSA search Engine/src/DataClass/Student.java
@@ -1,20 +1,43 @@
 package DataClass;
 
+import java.util.Date;
+
 public class Student {
+
     private String firstName;
     private String lastName;
     private long ic;
     private String eduBackground;
+    //<editor-fold desc="extra entities tbc">
+    private Date dateOfBirth;
+    private String contactNumber;
+    private String homeAddress;
+    private String emailAddress;
+    //</editor-fold>
 
     public Student() {
+
     }
-    
-    public Student(String firstName, String lastName, long ic ,String eduBackground) {
+
+    public Student(String firstName, String lastName, long ic, String eduBackground) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.ic = ic;
         this.eduBackground = eduBackground;
     }
+
+    //<editor-fold desc="extra constructor tbc">
+    public Student(String firstName, String lastName, long ic, String eduBackground, Date dateOfBirth, String contactNumber, String homeAddress, String emailAddress) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.ic = ic;
+        this.eduBackground = eduBackground;
+        this.dateOfBirth = dateOfBirth;
+        this.contactNumber = contactNumber;
+        this.homeAddress = homeAddress;
+        this.emailAddress = emailAddress;
+    }
+    //</editor-fold>
 
     public String getFirstName() {
         return firstName;
@@ -47,5 +70,38 @@ public class Student {
     public void setEduBackground(String eduBackground) {
         this.eduBackground = eduBackground;
     }
- 
+
+    //<editor-fold desc="extra get set method tbc">
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    public String getHomeAddress() {
+        return homeAddress;
+    }
+
+    public void setHomeAddress(String homeAddress) {
+        this.homeAddress = homeAddress;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+    //</editor-fold>
 }
--- DSA search Engine/src/SearchEngine/Main.java
+++ DSA search Engine/src/SearchEngine/Main.java
@@ -1,26 +1,53 @@
 package SearchEngine;
 
 import DataClass.*;
+import Operation.StudentDetail;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Main {
 
     public static void main(String[] args) {
         banner();
+        MenuOption();
     }
     
+    public static void MenuOption() {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Main Menu");
+        System.out.println("01. Student Detail");
+        System.out.println("Please Enter Menu Code");
+
+        String input = scan.nextLine();
+        
+        switch (input) {
+            case "01":
+                StudentDetail studentDetail = new StudentDetail();
+                studentDetail.Show();
+                break;
+            default:
+                MenuOption();
+                break;
+        }
+    }
+
     public static void banner() {
         System.out.println("-----------------------");
         System.out.println("   Welcome To TARUC    ");
         System.out.println("-----------------------");
     }
-    
+
     public static void clearScreen() {
         for (int i = 0; i < 50; i++) {
             System.out.println("");
         }
     }
-    
 }
