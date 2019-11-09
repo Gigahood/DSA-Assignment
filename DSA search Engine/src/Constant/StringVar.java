81ffbb2547e77e7f94e2da7120f55fd269678dd6
Wong Chia Zhen <wczhen1226@gmail.com>
2019-11-09 22:52:55 +0800

    implementation on view student detail

---
+++ DSA search Engine/src/Constant/StringVar.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Constant;
+
+/**
+ *
+ * @author jdczw
+ */
+public class StringVar {
+
+    public static final String LBL_STUDENT_DETAIL = "Student Detail";
+    public static final String LBL_STUDENT_ID = "Student ID : ";
+    public static final String LBL_FIRST_NAME = "First Name : ";
+    public static final String LBL_LAST_NAME = "Last Name : ";
+    public static final String LBL_IC_NUMBER = "IC Number : ";
+    public static final String LBL_STATUS = "Status : ";
+    public static final String LBL_CGPA = "CGPA : ";
+}
