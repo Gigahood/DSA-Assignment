6b35164f4bf664064e66bc42ba457e5aefef893d
siow man quan <siow_manquan97@hotmail.com>
2019-11-13 23:05:23 +0800

    search student id(Admin)

--- DSA search Engine/src/SearchEngine/AdminModule.java
+++ DSA search Engine/src/SearchEngine/AdminModule.java
@@ -62,7 +62,39 @@ public class AdminModule {
     }
     
     public void ShowSearchDetail(){
+        System.out.println("Please enter the student ID:");
+        String ID = Main.scan.nextLine();
         
-        
+        for (int index=0;index<Main.db.studentList.size();index++)
+        {
+            if (ID.equals(Main.db.studentList.get(index).getStudentID()))
+            {
+                System.out.println("-----------------------------------------------------");
+                System.out.println(String.format("|%-50s|", StringVar.LBL_STUDENT_DETAIL));
+                System.out.println("-----------------------------------------------------");
+                System.out.println(String.format("|%-50s|", StringVar.LBL_STUDENT_ID + Main.db.studentList.get(index).getStudentID()));
+                System.out.println(String.format("|%-50s|", StringVar.LBL_FIRST_NAME + Main.db.studentList.get(index).getFirstName()));
+                System.out.println(String.format("|%-50s|", StringVar.LBL_LAST_NAME + Main.db.studentList.get(index).getLastName()));
+                System.out.println(String.format("|%-50s|", StringVar.LBL_IC_NUMBER + Main.db.studentList.get(index).getIc()));
+                System.out.println(String.format("|%-50s|", StringVar.LBL_STATUS + Main.db.studentList.get(index).getStudyStatus()));
+                System.out.println(String.format("|%-50s|", StringVar.LBL_CGPA + Main.db.studentList.get(index).getCgpa()));
+                System.out.println("----------------------------------------------------");
+                System.out.println("Press Enter To Continue!");
+                Main.scan.nextLine();
+                System.out.println("");
+                System.out.println("");
+                break;
+            }
+            else
+            {
+               if (index==Main.db.studentList.size()-1)
+               {
+                    System.out.println("Student ID doesn't exist!");
+                    System.out.println("Please press again.");
+                    break;
+               }
+
+            }
+        }
     }
 }
