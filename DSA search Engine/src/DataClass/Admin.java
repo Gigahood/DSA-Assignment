5f82121547b90907c70edd320db87b7377c660de
styleluffy05 <52067865+styleluffy05@users.noreply.github.com>
2019-11-13 12:16:31 +0800

    Add files via upload

--- DSA search Engine/src/DataClass/Admin.java
+++ DSA search Engine/src/DataClass/Admin.java
@@ -6,16 +6,57 @@ package DataClass;
  * @author User
  */
 public class Admin {
+    private String gender;
+    private String name;
     private String username;
     private String password;
 
     public Admin() {
     }
 
-    public Admin(String username, String password) {
+    public Admin(String gender, String name, String username, String password) {
+        this.gender = gender;
+        this.name = name;
         this.username = username;
         this.password = password;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    
+
+    
+    
     
     
 }
--- DSA search Engine/src/SearchEngine/AdminModule.java
+++ DSA search Engine/src/SearchEngine/AdminModule.java
@@ -5,10 +5,96 @@
  */
 package SearchEngine;
 
+import Constant.StringVar;
+import DataClass.Admin;
+
 /**
  *
  * @author User
  */
 public class AdminModule {
     
+    private int index;
+    private String input;
+
+    public AdminModule() {
+    }
+
+    public AdminModule(int index) {
+
+        this.index = index;
+        
+        Navigation();
+    }
+    
+    public void Menu() {
+        Main.clearScreen();
+        System.out.println("------** Admin Page **-------");
+        System.out.println("Welcome, "+Main.db.adminList.get(index).getGender()+" "+Main.db.adminList.get(index).getName()+" ~");
+        System.out.println("1. Search Detail");
+        System.out.println("2. Return");
+        System.out.print("Your Selection ---> ");
+    }
+    
+    public void Navigation() {
+
+        while (true) {
+            while (true) {
+                Menu();
+                input = Main.scan.nextLine();
+
+                if (Main.checkInputMenu(2, input)) {
+                    break;
+                }
+            }
+
+            if (input.equals("2")) {
+                Main.clearScreen();
+                break;
+            }
+
+            switch (input) {
+                case "1":
+                    ShowSearchDetail();
+                    break;
+            }
+        }
+    }
+    
+    public void ShowSearchDetail(){
+        System.out.println("Please enter the student ID:");
+        String ID = Main.scan.nextLine();
+        
+        for (int index=0;index<Main.db.studentList.size();index++)
+        {
+            if (ID.equals(Main.db.studentList.get(index).getStudentID()))
+            {
+                System.out.println("-----------------------------------------------------");
+                System.out.println(String.format("|%-50s|", StringVar.LBL_STUDENT_DETAIL));
+                System.out.println("-----------------------------------------------------");
+                System.out.println(String.format("|%-50s|", StringVar.LBL_STUDENT_ID + Main.db.studentList.get(index).getStudentID()));
+                System.out.println(String.format("|%-50s|", StringVar.LBL_FIRST_NAME + Main.db.studentList.get(index).getFirstName()));
+                System.out.println(String.format("|%-50s|", StringVar.LBL_LAST_NAME + Main.db.studentList.get(index).getLastName()));
+                System.out.println(String.format("|%-50s|", StringVar.LBL_IC_NUMBER + Main.db.studentList.get(index).getIc()));
+                System.out.println(String.format("|%-50s|", StringVar.LBL_STATUS + Main.db.studentList.get(index).getStudyStatus()));
+                System.out.println(String.format("|%-50s|", StringVar.LBL_CGPA + Main.db.studentList.get(index).getCgpa()));
+                System.out.println("----------------------------------------------------");
+                System.out.println("Press Enter To Continue!");
+                Main.scan.nextLine();
+                System.out.println("");
+                System.out.println("");
+                break;
+            }
+            else
+            {
+               if (index==Main.db.studentList.size()-1)
+               {
+                    System.out.println("Student ID doesn't exist!");
+                    System.out.println("Please press again.");
+                    break;
+               }
+
+            }
+        }
+    }
 }
